8ce71beaaf6563d2b09aa794781104aaa9cbd9ef
unknown <frankfrankfrankyeyeye@gmail.com>
2019-09-07 01:38:10 -0400

    Implemented tracker communication funcs

--- client.py
+++ client.py
@@ -1,5 +1,6 @@
 import requests
 import socket
+import struct
 import ssl
 import json
 import time
@@ -14,6 +15,7 @@ P2P_PORT_NUMBER = 8081
 print("Hello world!")
 
 MAGIC_NUMBER_CLIENT = b"FRANK GAY"
+MAGIC_NUMBER_SERVER = b"SEAN. GAY"
 
 
 peer_list = set()
@@ -37,13 +39,21 @@ class TrackerHandler(BaseHTTPRequestHandler):
         self.wfile.write(response)
         peer_list.add(self.client_address[0])
 
+def QueryMessage(secure_sock: ssl.SSLSocket, message_id: str, buffer_size=1024):
+    secure_sock.send(MAGIC_NUMBER_CLIENT + b"2" + bytes(message_id, "utf-8"))
+    return list(map(socket.inet_ntoa, re.findall('....', secure_sock.recv(buffer_size).decode()[len(MAGIC_NUMBER_SERVER):])))
+
+
+def GetMessages(secure_sock: ssl.SSLSocket, buffer_size=1024, hash_size=32):
+    secure_sock.send(MAGIC_NUMBER_CLIENT + b"1")
+    return list(map(lambda x:x.hex(), re.findall('.'*hash_size, secure_sock.recv(buffer_size).decode()[len(MAGIC_NUMBER_SERVER):])))
+
 def main():
     while True:
         try:
             with socket.create_connection((SERVER, 8080)) as connection_sock:
                 with ssl_context.wrap_socket(connection_sock, server_hostname=SERVER) as secure_sock:
                     print(secure_sock.version())
-                    secure_sock.send(b"Frank gay")
                     print(secure_sock.recv(1024).decode())
             #poll = requests.get()
             time.sleep(POLL_INTERVAL/1000)
