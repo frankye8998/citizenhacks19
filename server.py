e8feefba01c3b489dd5c8000e0adec6792e3677a
unknown <frankfrankfrankyeyeye@gmail.com>
2019-09-08 04:44:59 -0400

    a

--- server.py
+++ server.py
@@ -7,6 +7,7 @@ from collections import OrderedDict
 import threading
 import certauth
 from certauth.certauth import *
+import threading
 
 HOST_NAME = "0.0.0.0" 
 PORT_NUMBER = 8080
@@ -14,11 +15,7 @@ REG_PORT = 8083
 print("Hello world!")
 
 messages = OrderedDict()
-#messages.__setitem__("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA", ["1.1.1.1"])
-#messages["AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA"].append("2.2.2.2")
-#messages.__setitem__("BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB", [])
-#messages["BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB"].append("3.3.3.3")
-#messages["BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB"].append("4.4.4.4")
+messages.__setitem__("A"*60, ["1.1.1.1"]) # This is such a big hack
 
 
 
@@ -43,20 +40,29 @@ def BytesToBcrypt(bcrypt_bytes: bytes):
     return "$2b$" + cost + "$" + bcrypt_string'''
 
 
-def CommandHandler(command: str, send):
+def CommandHandler(command: str, sender_ip):
     """Handles the command, NOT including magic number"""
     global messages
-    command = str(command)
     if command:
         if command[0] == '2': # Query message command
             messages[command[1:]].append(sender_ip)
-            return "".join(map(socket.inet_aton, messages[command[1:]]))
+            temp2 = ",".join(messages[command[1:]])
+            print(type(temp2))
+            
+            return bytes(temp2, "utf-8")
         elif command[0] == '1': # Get message command
-            return bytes.fromhex("".join(messages.keys()))
+            return bytes("".join(messages.keys()), "utf-8")
         else:
             raise NotImplemented(command[0])
     else:
         return
+
+def RegisterConn(conn, addr):
+    print("Registered {addr} to 8083!")
+    while True:
+        conn.send(HandleRegister(conn.recv(1024).decode(), addr[0]))
+        print(f"Registered a message from {addr}!")
+        
 
 def HandleRegister(message_id, sender_ip):
     global messages
@@ -69,25 +75,34 @@ def RegisterMessages():
         sock.listen(5)
         with context.wrap_socket(sock, server_side=True) as ssock:
             try:
-                conn, addr = ssock.accept()
-                print(conn, addr)
                 while True:
-                    temp = conn.recv(1024)
-                    print(temp)
-                    conn.send(HandleRegister(temp.decode(), addr[0]))
+                    conn, addr = ssock.accept()
+                    print(f"8083: Conn {addr}")
+                    threading.Thread(target=RegisterConn, args=(conn,addr,)).start()
             except KeyboardInterrupt:
                 pass 
 
 threading.Thread(target=RegisterMessages).start()
+
+def HandleNewConn(conn, addr):
+     print(f"New conn! {addr}")
+     while True:
+         temp = conn.recv(1024).decode()
+         print(f"Received {temp} from {addr}")
+         temp1 = CommandHandler(temp, addr[0])
+         print(temp1)
+         conn.send(temp1)
+         print(f"New packet from {addr}")
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM, 0) as sock:  
     sock.bind((HOST_NAME, PORT_NUMBER)) # Set up listening for socket
     sock.listen(5)  # Limit num. of connections simultaneously possible?
     with context.wrap_socket(sock, server_side=True) as ssock:
         try:
-            conn, addr = ssock.accept()
-            print(conn, addr)
             while True:
-                conn.send(CommandHandler(conn.recv(1024).decode(), addr[0]))
+                conn, addr = ssock.accept()
+                print(f"Accepted {addr}")
+                threading.Thread(target=HandleNewConn, args=(conn,addr,)).start()
+                    
         except KeyboardInterrupt:
             pass
